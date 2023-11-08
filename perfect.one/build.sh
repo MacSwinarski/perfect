000abac0faa1185c65483d4bb00454b39fe57657
mswinarski <mswinarski@atlassian.com>
2023-11-08 11:46:06 +0100

    new version with ready/live probe

--- perfect.one/build.sh
+++ perfect.one/build.sh
@@ -1,1 +1,5 @@
+echo "Building..."
 docker build -t mswinarski/perfect-one:latest -t mswinarski/perfect-one:$(git log -1 --pretty=%h) .
+echo "Pushing..."
+docker push mswinarski/perfect-one:$(git log -1 --pretty=%h)
+echo "Done. Now update perfect-one-pod.yaml to deploy the new image: mswinarski/perfect-one:$(git log -1 --pretty=%h)"
